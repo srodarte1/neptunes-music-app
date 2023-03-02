30f6dee133719376ae04c3157174b1bf7d543de3
+ <rodarte_sarah@yahoo.com>
2023-03-01 17:37:15 -0700

    forgot the playlist song controller

--- app/controllers/playlist_songs_controller.rb
+++ app/controllers/playlist_songs_controller.rb
@@ -1,3 +1,25 @@
 class PlaylistSongsController < ApplicationController
   #create and destroy
+
+    def create
+        render json: PlaylistSong.create!(playlist_params), status: :created
+    end
+
+    def destroy 
+        @playlist_song.destroy
+        head :no_content
+    end
+
+    private
+    private 
+
+    def find_playlist_song
+    @playlist_song = PlaylistSong.find(params[:id])
+    end
+    
+    def playlist_song_params 
+    params.permit(:playlist, :song)
+    end
 end
+
+
